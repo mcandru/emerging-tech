f3323c13cd7e2fba0cdb5c846421964a59e2a44a
Michael McAndrew <michaeltmcandrew@gmail.com>
2026-01-23 12:23:07 +0000

    Save progress

---
+++ 03-coding-with-ai/tab-complete/tempConversion.js
@@ -0,0 +1,6 @@
+// TODO: Implement temperature conversion functions
+
+// Test the functions
+console.log("0°C is " + celsiusToFahrenheit(0) + "°F");
+console.log("100°C is " + celsiusToFahrenheit(100) + "°F");
+console.log("32°F is " + fahrenheitToCelsius(32) + "°C");
